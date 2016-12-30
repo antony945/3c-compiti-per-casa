27cb159c56904dfefffa55b8d5a65c01774ea986
antony945 <ant.frank10@yahoo.it>
2016-12-30 22:00:43 +0100

    MODIFICATO CODICE GIOCO

--- C++/Game/Capra, lupo, cavolo/main.cpp
+++ C++/Game/Capra, lupo, cavolo/main.cpp
@@ -1,5 +1,22 @@
 //Franzoso Antonio 3C Capra, Lupo, Cavolo
 
+//-------------------------------------------------------------------------------------------------------------------------------
+/* MODIFICHE DA VERSIONE PRECEDENTE:
+   - MODIFICATO CICLO PRINCIPALE DEL GIOCO. DA CICLO 'while' A CICLO 'for' AL FINE DI OTTIMIZZARE IL GIOCO.
+
+   - AGGIUNTA NUOVA FUNZIONE 'transizioneBarca()' CHE SERVE A FAR VISUALIZZARE LO SPOSTAMENTO DELLA BARCA DOPO AVER SCELTO LA MOSSA.
+
+   - ELIMINATE VARIABILI BOOLEANE 'vittoria' E 'sconfitta' RITENUTE SUPERFLUE, DATO IL CONTROLLO DELLO STATO DEL GIOCO
+     (se VINTO o PERSO) TRAMITE LE FUNZIONI 'controllaVittoria()' E 'controllaSconfitta()'.
+
+   - OTTIMIZZATO FUNZIONAMENTO VARIABILI DEGLI ELEMENTI NELLO SCENARIO.
+
+   - ELIMINATA COMPLETAMENTE 'barca' COME ELEMENTO SELEZIONABILE. ESSA SVOLGE SOLO RUOLO GRAFICO.
+
+   - ELIMINATA VARIABILE LATO_BARCA, CAUSA IMPLEMENTAZIONE CICLO FOR CON CONTATORE PER REGOLARE LO STATO DEL GIOCO.
+     SE CONTATORE HA VALORE DISPARI, LA BARCA E' A DESTRA; SE PARI, A SINISTRA. */
+//-------------------------------------------------------------------------------------------------------------------------------
+
 #include <iostream>
 #include <stdlib.h>
 
@@ -14,11 +31,15 @@ void messaggioIniziale(void);
 
 void messaggioFinale(void);
 /* Stampa a video il messaggio che riceve l'utente dopo la fine del gioco.
-   Varia a seconda del valore delle variabili booleane 'vittoria' e 'sconfitta'. */
+   Varia a seconda del valore ritornato dalle funzioni 'controllaVittoria()' e 'controllaSconfitta()'. */
 
 void inizializzaGioco(void);
 /* Mostra il messaggio iniziale e inizializza tutte le variabili del gioco
    assegnando loro il valore desiderato prima che inizi il gioco. */
+
+void transizioneBarca(void);
+/* Stampa a video l'elemento grafico della barca che cambia sponda dopo ogni turno.
+   RICORDA! Quando CONTATORE = 1, NON deve essere richiamata. */
 
 void disegnaScenario(void);
 /* Stampa a video lo scenario nel momento in cui viene richiamata
@@ -30,6 +51,11 @@ void interazioneUtente(void);
 
 void eseguiAzione(void);
 /* CAMBIA il valore delle variabili a seconda delle scelte fatte nella funzione 'interazioneUtente'. */
+
+bool controllaFine();
+/* Controlla se, dopo aver applicato i cambiamenti imposti dall'utente alle variabili,
+   il gioco è terminato. Può restituire 'TRUE' (terminato), o 'FALSE' (non terminato).
+   Si compone del valore ritornato dalle funzione controllaVittoria() e controllaSconfitta(). */
 
 bool controllaVittoria();
 /* Controlla se, dopo aver applicato i cambiamenti imposti dall'utente alle variabili,
@@ -44,53 +70,31 @@ bool controllaSconfitta();
 /* Lista di tutte le variabili dichiarate GLOBALMENTE che verranno usate nel codice di gioco.
    Sono coloro che potranno essere usate nel 'main' e all'interno di tutte le funzioni richiamate nel 'main'. */
 
-bool vittoria;
-/* Variabile booleana che restituisce 'TRUE' se l'utente VINCE il gioco attraverso le azioni
-   compiute, altrimenti 'FALSE'. Si ha vittoria quando controllaVittoria() restituisce 'TRUE'. */
-
-bool sconfitta;
-/* Variabile booleana che restituisce 'TRUE' se l'utente PERDE il gioco attraverso le azioni
-   compiute, altrimenti 'FALSE'. Si ha sconfitta quando controllaSconfitta() restituisce 'TRUE'. */
-
 bool end_game;
 /* Variabile booleana che restituisce 'TRUE' se il gioco termina a causa di un cambiamento di
    scenario imposto dall'utente, altrimenti 'FALSE'. Il gioco finisce quando vittoria = TRUE o sconfitta = TRUE.
    Variabile utilizzata nel loop principale del gioco. */
 
-bool capra_dx; //Assume 'TRUE' se è presente una capra nella sponda dx, altrimenti 'FALSE'
-bool capra_sx; //Assume 'TRUE' se è presente una capra nella sponda sx, altrimenti 'FALSE'
-bool capra_barca; //Assume 'TRUE' se è presente una capra nella barca, altrimenti 'FALSE'
-/* Variabili booleane rappresentanti le capre presenti nello scenario. Assumono valore 'TRUE' se ci sono nella determinata
-   sponda o barca espressa dal nome, altrimenti 'FALSE' se non ci sono nella determinata sponda o barca espressa dal nome.
-   Se una delle tre variabili assume 'TRUE', è necessario che le altre assumano 'FALSE'. */
+bool capra_sponda;
+/* Variabile booleana indicante la posizione della CAPRA nello scenario.
+   Assume 'TRUE' se la CAPRA è nella sponda sx, ovvero nella posizione dove il gioco richiede che sia, altrimenti 'FALSE'. */
 
-bool lupo_dx; //Assume 'TRUE' se è presente un lupo nella sponda dx, altrimenti 'FALSE'
-bool lupo_sx; //Assume 'TRUE' se è presente un lupo nella sponda sx, altrimenti 'FALSE'
-bool lupo_barca; //Assume 'TRUE' se è presente un lupo nella barca, altrimenti 'FALSE'
-/* Variabili booleane rappresentanti i lupi presenti nello scenario. Assumono valore 'TRUE' se ci sono nella determinata
-   sponda o barca espressa dal nome, altrimenti 'FALSE' se non ci sono nella determinata sponda o barca espressa dal nome.
-   Se una delle tre variabili assume 'TRUE', è necessario che le altre assumano 'FALSE'. */
+bool lupo_sponda;
+/* Variabile booleana indicante la posizione del LUPO nello scenario.
+   Assume 'TRUE' se il LUPO è nella sponda sx, ovvero nella posizione dove il gioco richiede che sia, altrimenti 'FALSE'. */
 
-bool cavolo_dx; //Assume 'TRUE' se è presente un cavolo nella sponda dx, altrimenti 'FALSE'
-bool cavolo_sx; //Assume 'TRUE' se è presente un cavolo nella sponda sx, altrimenti 'FALSE'
-bool cavolo_barca; //Assume 'TRUE' se è presente un cavolo nella barca, altrimenti 'FALSE'
-/* Variabili booleane rappresentanti i cavoli presenti nello scenario. Assumono valore 'TRUE' se ci sono nella determinata
-   sponda o barca espressa dal nome, altrimenti 'FALSE' se non ci sono nella determinata sponda o barca espressa dal nome.
-   Se una delle tre variabili assume 'TRUE', è necessario che le altre assumano 'FALSE'. */
+bool cavolo_sponda;
+/* Variabile booleana indicante la posizione del CAVOLO nello scenario.
+   Assume 'TRUE' se il CAVOLO è nella sponda sx, ovvero nella posizione dove il gioco richiede che sia, altrimenti 'FALSE'. */
 
-char lato_barca;
-/* Variabile che assume solamente due valori:
-   - 'd' se la barca è attraccata alla sponda dx
-   - 's' se la barca è attraccata alla sponda sx */
-
-char scelta_elemento;
-/* Variabile che memorizza la scelta effettuata in input dal giocatore su quale elemento far muovere.
-   Può assumere quattro valori:
-   - 'G' o 'g' per capra; la capra salirà o scenderà dalla barca a seconda della sua posizione
-                          (se si trova su una sponda salirà, altrimenti scenderà dalla barca)
+char scelta;
+/* Variabile che memorizza la scelta effettuata in input dal giocatore su quale elemento far muovere da una sponda all'altra.
+   (lo spostamento verrà stampato a video con l'ausilio della nuova funzione 'transizioneBarca()'
+   Può assumere tre valori:
+   - 'G' o 'g' per capra; la capra si spostesa sulla sponda opposta a seconda della sua posizione di partenza
+                          (se si trova sulla sponda dx passerà sulla sponda sx, altrimenti passerà sulla sponda dx)
    - 'L' o 'l' per lupo; vedi funzionamento capra
-   - 'C' o 'c'per cavolo; vedi funzionamento capra
-   - 'B' o 'b' per barca; la barca partirà dalla sponda nella quale si trova, arrivando a quella opposta */
+   - 'C' o 'c'per cavolo; vedi funzionamento capra */
 
 //*******************************************************************************************************************************
 /* CODICE PROGRAMMA */
@@ -98,12 +102,16 @@ char scelta_elemento;
 int main(){
     inizializzaGioco();
 
-    while(!end_game){
+    for(int mosse = 1; !end_game; mosse++){
+        Transizione();
         disegnaScenario();
+
+        if(controllaFine()){
+            continue;
+        }
+
         interazioneUtente();
         eseguiAzione();
-        controllaVittoria();
-        controllaSconfitta()
     }
 
     messaggioFinale();
@@ -112,26 +120,37 @@ int main(){
     return 0;
 }
 
-messaggioIniziale(){
+void messaggioIniziale(){
 }
 
-messaggioFinale(){
+void messaggioFinale(){
 }
 
-inizializzaGioco(){
+void inizializzaGioco(){
 }
 
-disegnaScenario(){
+void Transizione(){
 }
 
-interazioneUtente(){
+void disegnaScenario(){
 }
 
-eseguiAzione(){
+void interazioneUtente(){
 }
 
-controllaVittoria(){
+void eseguiAzione(){
 }
 
-controllaSconfitta(){
+bool controllaFine(){
+    bool controlla_fine = FALSE;
+
+    controlla_fine = (controllaVittoria() || controllaSconfitta());
+
+    return controlla_fine;
+}
+
+bool controllaVittoria(){
+}
+
+bool controllaSconfitta(){
 }
