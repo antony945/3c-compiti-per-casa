efbfc220612704a1b92c2337789f36c23f194d0b
antony945 <ant.frank10@yahoo.it>
2017-02-17 11:06:46 +0100

    ggiunta

--- C++/Esercizi/Ricorsivita - Es. 22.cpp
+++ C++/Esercizi/Ricorsivita - Es. 22.cpp
@@ -7,3 +7,16 @@ int main(){
 
     return 0;
 }
+
+m*1000 c*100 d*10 u*1
+
+m=num/1000
+c=(num%1000)/100
+d=((num%1000)%100)/10
+u =((num%1000)%100)%10
+
+int x = 1000
+
+if(num = una cifra)
+    for(i=0; i<CIFRE_TOTALI; i++)
+        valori(num%(x/10));
