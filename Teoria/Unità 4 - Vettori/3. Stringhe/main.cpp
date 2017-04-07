05fa1090596443c7fe899992f05fddc26bf34220
antony945 <antonio.franzoso@outlook.it>
2017-04-07 11:03:24 +0200

    Stringhe

---
+++ Teoria/Unità 4 - Vettori/3. Stringhe/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    char metodo1[] = {'M','a','r','c','o', '\0'};
+    char metodo2[] = "Volpi";
+
+    cout << metodo1 << " " << metodo2 << endl;
+
+    system("PAUSE");
+}
